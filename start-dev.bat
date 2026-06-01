7b6216d848fe72b45c79b86c58ef0963cc655d33
RamiroBogado <federama2@gmail.com>
2026-06-01 14:39:19 -0300

    refactor: start-dev.bat

--- start-dev.bat
+++ start-dev.bat
@@ -1,6 +1,20 @@
 @echo off
 
 echo =========================
+echo Instalando dependencias del Backend...
+echo =========================
+
+call mvn clean install -DskipTests
+
+echo =========================
+echo Instalando dependencias del Frontend...
+echo =========================
+
+cd frontend
+call npm install
+cd ..
+
+echo =========================
 echo Levantando Backend...
 echo =========================
 
@@ -23,7 +37,7 @@ echo =========================
 start http://localhost:3000
 
 echo =========================
-echo Proyecto iniciado
+echo Proyecto iniciado correctamente
 echo Backend: http://localhost:8080
 echo Frontend: http://localhost:3000
 echo =========================
